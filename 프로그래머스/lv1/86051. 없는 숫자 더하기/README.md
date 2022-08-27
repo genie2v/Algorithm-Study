da97af1748d06ef304fcc5ee53698ac8b3c5e3a7
진이 <82151186+genie2v@users.noreply.github.com>
2022-08-27 22:11:10 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.01 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 없는 숫자 더하기 - 86051 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/86051) 
 
 ### 성능 요약
 
-메모리: 72.4 MB, 시간: 0.02 ms
+메모리: 85.6 MB, 시간: 0.01 ms
 
 ### 구분
 
--- 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/lv1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,7 +1,8 @@
 class Solution {
     public int solution(int[] numbers) {
+        // 방법1
         int answer = 0;
-        for(int i=0; i<=9;i++){
+        /* for(int i=0; i<=9;i++){
             int count = 0;
             for(int j=0; j<numbers.length;j++){
                 if(i == numbers[j]){
@@ -9,13 +10,14 @@ class Solution {
                 }
             }
             if(count == 0) answer += i;
-        }
+        } */
         
-        // int sum = 45;
-        // for(int i=0; i<numbers.length; i++){
-        //     sum -= numbers[i];
-        // }
-        //answer = sum;
+        // 방법2 총합45에서 있는 숫자를 빼는 방법
+        int sum = 45;
+        for(int i=0; i<numbers.length; i++){
+            sum -= numbers[i];
+        }
+        answer = sum;
         return answer;
     }
 }
