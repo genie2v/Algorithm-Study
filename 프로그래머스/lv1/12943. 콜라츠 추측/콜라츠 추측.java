4a83aadcffe2d3c27e0003760075c5d1bf25e3c9
진이 <82151186+genie2v@users.noreply.github.com>
2022-08-27 22:05:57 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.06 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int solution(int num) {
+        int answer = 0;
+        long n = Long.valueOf(num);
+        
+        while(n!=1){
+            answer++;
+            if(answer == 500) {
+                answer = -1;
+                break;
+            }
+            n = ((n%2)==0) ? n/2 : n*3+1;
+        }
+        
+        return answer;
+    }
+}
