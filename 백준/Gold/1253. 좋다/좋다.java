e68246e2b50a35560af073ddf5804e02e81f3ff9
진이 <82151186+genie2v@users.noreply.github.com>
2022-08-28 22:32:57 +0900

    [Gold IV] Title: 좋다, Time: 172 ms, Memory: 14828 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.java
+++ 백준/Gold/1253. 좋다/좋다.java
@@ -5,9 +5,6 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-//	N개의 수 중에서 어떤 수가 다른 수 두 개의 합으로 나타낼 수 있다면 그 수를 “좋다(GOOD)”고 한다.
-//	N개의 수가 주어지면 그 중에서 좋은 수의 개수는 몇 개인지 출력하라.
-//	수의 위치가 다르면 값이 같아도 다른 수이다.
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		// TODO Auto-generated method stub
@@ -20,6 +17,7 @@ public class Main {
 			a[i] = Integer.parseInt(stringTokenizer.nextToken());
 		}
 		Arrays.sort(a);
+        
 		for (int k = 0; k < n; k++) {
 			int find = a[k];
 			int i = 0;
