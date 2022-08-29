3654bdfc09dd027c3a3ea59bec5a8a0d3a14d9a3
진이 <82151186+genie2v@users.noreply.github.com>
2022-08-29 13:58:47 +0900

    [level 1] Title: 3진법 뒤집기, Time: 13.00 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -7,12 +7,8 @@ class Solution {
             base3 += n%3;
             n /= 3;
         }
-        // base3 0021 /0*9+0*3+2*3+1*1=7
-        char[] chBase3 = base3.toCharArray();
-        for(int i=chBase3.length-1,j=0;i>=0;i--) {
-            answer += Character.getNumericValue(chBase3[i]) * (int)Math.pow(3, j);
-            j++;
-        }
+
+        answer = Integer.parseInt(base3,3);
         return answer;
     }
 }
--- 프로그래머스/lv1/68935. 3진법 뒤집기/README.md
+++ 프로그래머스/lv1/68935. 3진법 뒤집기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 3진법 뒤집기 - 68935 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/68935?language=java) 
 
 ### 성능 요약
 
-메모리: 77.9 MB, 시간: 9.15 ms
+메모리: 82 MB, 시간: 13.00 ms
 
 ### 구분
 
