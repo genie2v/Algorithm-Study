e8a8f377ec147ac319dae2be9dd909cb387f4da4
진이 <82151186+genie2v@users.noreply.github.com>
2022-08-31 12:47:44 +0900

    [level 1] Title: 모의고사, Time: 1.58 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    // 수포자 1 : 12345
+    // 수포자 2 : 21232425
+    // 수포자 3 : 3311224455
+    public int[] solution(int[] answers) {
+        int user1[] = {1, 2, 3, 4, 5};
+        int user2[] = {2, 1, 2, 3, 2, 4, 2, 5};
+        int user3[] = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int score[] = new int[3];
+        ArrayList<Integer> arrList = new ArrayList<Integer>();
+        
+        for(int i=0;i<answers.length;i++) {
+            if(answers[i]==user1[i%user1.length]) score[0]++;
+            if(answers[i]==user2[i%user2.length]) score[1]++;
+            if(answers[i]==user3[i%user3.length]) score[2]++;
+        }
+        
+        int max = Math.max(score[0],Math.max(score[1],score[2]));
+        for(int i=0;i<3;i++) {
+            if(max==score[i]) arrList.add(i+1);
+        }
+        System.out.println(arrList);
+        int[] answer = new int[arrList.size()];
+        for(int i=0;i<answer.length;i++) {
+            answer[i] = arrList.get(i);
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
