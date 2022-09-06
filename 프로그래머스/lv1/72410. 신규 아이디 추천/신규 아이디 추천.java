d714c65f48e7aecdd3157b11f5a1f8646998abd8
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-06 16:24:54 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.50 ms, Memory: 65.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String new_id) {
+        String answer = "";
+        // 1단계
+        new_id = new_id.toLowerCase();
+        // 2단계
+        new_id = new_id.replaceAll("[^a-z0-9-_.]","");
+        // 3단계
+        new_id = new_id.replaceAll("\\.{2,}",".");
+        // 4단계
+        new_id = new_id.replaceAll("^\\.","");
+        new_id = new_id.replaceAll("\\.$","");
+        // 5단계
+        if(new_id.length()==0) new_id = "a";
+        // 6단계
+        if(new_id.length()>=16){
+            new_id = new_id.substring(0,15);
+            new_id = new_id.replaceAll("\\.$","");
+        }
+        // 7단계
+        if(new_id.length()<=2) {
+            int len = new_id.length();
+            String lastChar = new_id.substring(new_id.length()-1);
+            for(int i=0;i<3-len;i++) {
+                new_id += lastChar;
+            }
+        }
+
+        answer = new_id;
+        return answer;
+    }
+}
