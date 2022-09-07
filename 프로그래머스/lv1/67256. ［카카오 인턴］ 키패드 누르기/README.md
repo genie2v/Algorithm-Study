30a2120a1438c30eb7dfd9a881f8fb4f323e98a8
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-07 13:29:33 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 3.53 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/README.md
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/README.md
@@ -1,10 +1,10 @@
 # [level 1] [카카오 인턴] 키패드 누르기 - 67256 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/67256) 
 
 ### 성능 요약
 
-메모리: 76.3 MB, 시간: 3.68 ms
+메모리: 84.9 MB, 시간: 3.53 ms
 
 ### 구분
 
--- 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/lv1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,38 +1,39 @@
 class Solution {
     public String solution(int[] numbers, String hand) {
         String answer = "";
-        int leftHand = 10;
-        int rightHand = 12;
+        // 0,1,2,3,4,5,6,7,8,9
+        int[][] number = {{3,1},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+        int[] leftHand = {3,0};
+        int[] rightHand = {3,2};
         
         for(int num : numbers) {
             if(num==1||num==4||num==7) {
                 answer += "L";
-                leftHand = num;
+                leftHand = number[num];
             } else if(num==3||num==6||num==9) {
                 answer += "R";
-                rightHand = num;
+                rightHand = number[num];
             } else {
-                if(num == 0) num = 11;
-                int leftDist = (Math.abs(num-leftHand) / 3) + (Math.abs(num-leftHand) % 3);
-                int rightDist = (Math.abs(num-rightHand) / 3) + (Math.abs(num-rightHand) % 3);
+                int leftDist = Math.abs(number[num][0]-leftHand[0]) + Math.abs(number[num][1]-leftHand[1]);
+                int rightDist = Math.abs(number[num][0]-rightHand[0]) + Math.abs(number[num][1]-rightHand[1]);
                 if(leftDist<rightDist) {
                     answer += "L";
-                    leftHand = num;
+                    leftHand = number[num];
                 } else if(leftDist>rightDist) {
                     answer += "R";
-                    rightHand = num;
+                    rightHand = number[num];
                 } else {
                     if(hand.equals("right")) {
                         answer += "R";
-                        rightHand = num;
+                        rightHand = number[num];
                     } else {
                         answer += "L";
-                        leftHand = num;
+                        leftHand = number[num];
                     }
                 }
             }
         }
-        
+
         return answer;
     }
 }
