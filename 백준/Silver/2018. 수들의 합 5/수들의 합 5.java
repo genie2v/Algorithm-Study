753177f3369c8adc1df52bbfe7e6350773624894
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-13 13:09:28 +0900

    [Silver V] Title: 수들의 합 5, Time: 248 ms, Memory: 17740 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-//	어떠한 자연수 N은, 몇 개의 연속된 자연수의 합으로 나타낼 수 있다. 
-//	당신은 어떤 자연수 N(1 ≤ N ≤ 10,000,000)에 대해서, 이 N을 몇 개의 연속된 자연수의 합으로 나타내는 가지수를 알고 싶어한다. 
-//	이때, 사용하는 자연수는 N이하여야 한다.
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
