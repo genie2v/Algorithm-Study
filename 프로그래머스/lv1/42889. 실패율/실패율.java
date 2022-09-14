dd965fe24c760a1605bfe33600ae3a7af4a23a31
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-14 15:55:50 +0900

    [level 1] Title: 실패율, Time: 2.49 ms, Memory: 69.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42889. 실패율/실패율.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int N, int[] stages) {
+        int[] answer = new int[N];
+        HashMap<Integer,Double> hashMap = new HashMap<Integer,Double>();
+        int player = stages.length;
+        
+        for(int i=1;i<=N;i++) {
+            int failUsr = 0;
+            for(int j=0;j<stages.length;j++) {
+                if(stages[j]==i) failUsr++;
+            }
+            double fail = (failUsr==0) ? 0.0 : failUsr/(double)player;
+            hashMap.put(i,fail);
+            player = player - failUsr;
+        }
+        
+        ArrayList<Integer> listKeySet = new ArrayList<Integer>(hashMap.keySet());
+        Collections.sort(listKeySet, (value1, value2) -> (hashMap.get(value2).compareTo(hashMap.get(value1))));
+        int idx = 0;
+        for(int key : listKeySet) {
+            //System.out.println(key + " : " + hashMap.get(key));
+            answer[idx++] = key;
+        }
+
+        return answer;
+    }
+}
