072fff2717483e21bbe098b55da869183cec0c41
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-19 15:54:15 +0900

    [level 2] Title: [1차] 캐시, Time: 2.37 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int cacheSize, String[] cities) {
+        int answer = 0;
+        LinkedList<String> cache = new LinkedList<String>();
+        
+        if(cacheSize==0) return cities.length * 5;
+        
+        for(int i=0;i<cities.length;i++) {
+            String city = cities[i].toLowerCase();
+            
+            // cache hit
+            if(cache.remove(city)) {
+                cache.addFirst(city);
+                answer += 1;
+            } else {
+                // cache miss
+                if(cache.size()==cacheSize) {
+                    cache.removeLast();
+                }
+                cache.addFirst(city);
+                answer += 5;
+            }
+        }
+        
+        return answer;
+    }
+}
