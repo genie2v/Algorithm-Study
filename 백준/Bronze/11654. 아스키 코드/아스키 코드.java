5189cd10d0d6b8ff3aea03bd37c91e15b7bb1534
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-20 12:53:34 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		char ch = scanner.next().charAt(0);
+		int code = (int)ch;
+		System.out.println(code);
+		scanner.close();
+	}
+
+}
