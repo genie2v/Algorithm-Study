c2cb6683179f8d175979425ba7bb9a7adb9d498b
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-21 12:55:36 +0900

    [level 2] Title: H-Index, Time: 19.80 ms, Memory: 85.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        
+        // 내림차순 정렬
+        for(int i=0;i<citations.length-1;i++) {
+            for(int j=0;j<citations.length-1;j++) {
+                if(citations[j] < citations[j+1]) {
+                    int temp = citations[j];
+                    citations[j] = citations[j+1];
+                    citations[j+1] = temp;
+                }
+            }
+        }
+        
+        for(int n : citations) System.out.print(n + " ");
+        
+        for(int i=1;i<=citations.length;i++) {
+            if(citations[i-1]<i) {
+                answer = i-1;
+                break;
+            } else {
+                answer = citations.length;
+            }
+        }
+        return answer;
+    }
+}
