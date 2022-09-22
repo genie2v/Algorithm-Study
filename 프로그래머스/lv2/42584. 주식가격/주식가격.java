580ea6a985be4bef7dbe052e022a56c75f99c1ae
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-22 11:18:06 +0900

    [level 2] Title: 주식가격, Time: 61.46 ms, Memory: 71.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
+        for(int price : prices) {
+            queue.add(price);
+        }
+        
+        int idx = 0;
+        while(!queue.isEmpty()) {
+            int time = 0;
+            Iterator iterator = queue.iterator();
+            int currentPrice = queue.peek();
+            for(int price : queue) {
+                time++;
+                if(price < currentPrice) break;
+            }
+            answer[idx] = time - 1;
+            idx++;
+            queue.poll(); 
+        }
+        
+        return answer;
+    }
+}
