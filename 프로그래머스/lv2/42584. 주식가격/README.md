e3561b9885902846da2ebcaaa0148342f0520089
진이 <82151186+genie2v@users.noreply.github.com>
2022-09-22 13:03:33 +0900

    [level 2] Title: 주식가격, Time: 25.33 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42584. 주식가격/README.md
+++ 프로그래머스/lv2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584?language=java) 
 
 ### 성능 요약
 
-메모리: 64.3 MB, 시간: 27.00 ms
+메모리: 78.4 MB, 시간: 25.33 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42584. 주식가격/주식가격.java
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.java
@@ -1,5 +1,6 @@
 import java.util.*;
 
+// Stack
 class Solution {
     public int[] solution(int[] prices) {
         int[] answer = new int[prices.length];
@@ -21,3 +22,32 @@ class Solution {
         return answer;
     }
 }
+
+// Queue
+/* class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Queue<Integer> queue = new LinkedList<Integer>();
+        
+        for(int price : prices) {
+            queue.add(price);
+        }
+        
+        int idx = 0;
+        while(!queue.isEmpty()) {
+            int time = 0;
+            int currentPrice = queue.peek();
+            for(int price : queue) {
+                time++;
+                // 가격이 떨어지면 break
+                if(price < currentPrice) break;
+            }
+            // 순회하면서 자기자신도 포함했기 때문에 자기자신은 빼준다
+            answer[idx] = time - 1;
+            idx++;
+            queue.poll(); 
+        }
+        
+        return answer;
+    }
+} */
