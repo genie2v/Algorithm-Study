408a06322bb5111df193bae375460a3a55df06ba
진이 <82151186+genie2v@users.noreply.github.com>
2023-07-12 13:37:53 +0900

    [unrated] Title: 취소되지 않은 진료 예약 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
@@ -0,0 +1,15 @@
+-- 코드를 입력하세요
+SELECT A.APNT_NO
+     , B.PT_NAME
+     , A.PT_NO
+     , A.MCDP_CD
+     , C.DR_NAME
+     , A.APNT_YMD
+  FROM APPOINTMENT A
+     , PATIENT B
+     , DOCTOR C
+ WHERE A.PT_NO = B.PT_NO
+   AND A.MDDR_ID = C.DR_ID
+   AND TO_CHAR(A.APNT_YMD,'YYYYMMDD') = '20220413'
+   AND A.APNT_CNCL_YN = 'N'
+ ORDER BY A.APNT_YMD
