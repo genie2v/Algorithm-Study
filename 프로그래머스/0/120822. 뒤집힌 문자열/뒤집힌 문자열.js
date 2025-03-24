668e17eda9837d9e9acefc3392246bcb3c2923d1
진이 <82151186+genie2v@users.noreply.github.com>
2025-03-24 15:52:21 +0900

    [level 0] Title: 뒤집힌 문자열, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
+++ 프로그래머스/0/120822. 뒤집힌 문자열/뒤집힌 문자열.js
@@ -1,10 +1,4 @@
 function solution(my_string) {
-    var answer = '';
-    let len = my_string.length;
-    
-    for (let i = len - 1; i >= 0; i--) {
-        answer += my_string.charAt(i);
-    }
-    
+    var answer = my_string.split('').reverse().join('');
     return answer;
 }
